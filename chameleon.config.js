d1bca842b7207d16e6e208e78844979a4e8dfde0
JiM-W <550116990@qq.com>
2019-07-18 11:12:53 +0800

    update .gitignore

--- chameleon.config.js
+++ chameleon.config.js
@@ -3,13 +3,36 @@
 const publicPath = 'https://chameleon-team.github.io/cml-yanxuan/dist';
 // 设置api请求前缀
 const apiPrefix = 'https://chameleon-team.github.io';
-
+const path = require('path');
 cml.config.merge({
   templateLang: "cml",
   templateType: "html",
-  platforms: ["web","baidu","alipay","wx","weex"],
+  // platforms: ["web","baidu","alipay","wx","weex"],
+  platforms: ['tt'],
   buildInfo: {
     wxAppId: '123456'
+  },
+  builtinNpmName: 'cml-tt-ui-builtin',
+  extPlatform: {
+    tt: 'cml-tt-plugin',
+  },
+  babelPath: [
+    path.join(__dirname,'node_modules/cml-tt-ui-builtin'),
+    path.join(__dirname,'node_modules/cml-tt-runtime'),
+    path.join(__dirname,'node_modules/cml-tt-api'),
+    path.join(__dirname,'node_modules/cml-tt-ui'),
+    path.join(__dirname,'node_modules/cml-tt-store'),
+    path.join(__dirname,'node_modules/cml-tt-mixins'),
+    path.join(__dirname,'node_modules/mobx'),
+  ],
+  baseStyle:{
+    wx: true,
+    web: true,
+    weex: true,
+    alipay: true,
+    baidu: true,
+    qq: true,
+    tt:true,
   },
   wx: {
     dev: {
